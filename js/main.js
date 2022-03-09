5fed92ff014eb9dbfd8cfc1260c88d773dbe2e6b
unknown <hellyeah1506@gmail.com>
2022-03-09 19:16:09 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function member (
+    id,
+    name,
+    email,
+    password,
+    datepicker,
+    luongCB,
+    chucvu,
+){
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.datepicker = datepicker;
+    this.luongCB = luongCB;
+    this.chucvu = chucvu;
+}
